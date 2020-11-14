cfc70ea0a5678b3569d09c75dcb9ca6603630977
Nguyen Duc Hoang <sunlight4d@gmail.com>
2020-11-14 20:36:56 +0700

    add

---
+++ dotnetCore5WebApi/FlightManager/Models/Flight.cs
@@ -0,0 +1,11 @@
+namespace FlightManager.Models
+ {
+     public class Flight
+     {
+         public int Id { get ;set; }
+         public string Origin { get; set; }
+         public string Destination { get; set; }
+         public string FlightNumber { get; set; }
+         public DateTime DepartureTime { get; set; }
+     }
+ }
---
+++ dotnetCore5WebApi/FlightManager/Models/FlightContext.cs
@@ -0,0 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+public class FlightsContext : DbConText
+{
+
+}
